9cb3030ba7ae9363a2680b176c3b261c84e3ee6a
MarcosCosmos <m.cosmos42@gmail.com>
2026-06-12 11:28:59 +1000

    delete extraneous files because my system doesn't seem to be able to move, only copy

--- src/components/Old/ItemLabel/ItemLabel.jsx
+++
@@ -1,9 +0,0 @@
-import OutlinedText from '../OutlinedText/OutlinedText';
-
-const ItemLabel = ({ className, children }) => (
-  <div className={`item-label ${className || ''}`}>
-    <OutlinedText>{children}</OutlinedText>
-  </div>
-);
-
-export default ItemLabel;
