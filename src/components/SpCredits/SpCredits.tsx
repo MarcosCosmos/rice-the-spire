303da6011080e9d848a9e1b46dd42dc89bb7e9b4
MarcosCosmos <m.cosmos42@gmail.com>
2026-06-14 12:28:46 +1000

    turns out zebar doesn't support links

--- src/components/SpCredits/SpCredits.tsx
+++ src/components/SpCredits/SpCredits.tsx
@@ -7,26 +7,24 @@ const SpCredits = () => {
   const label = "Credits";
   const tooltip = (
     <>
-      <h1>Bar author: </h1>
-      <a href="https://github.com/MarcosCosmos">
-        <SpSpireImage path="ui/characters/character_icon_defect" />
-        MarcosCosmos
-      </a>
+      <h1>Author: </h1>
+      <SpSpireImage path="ui/characters/character_icon_defect" />
+      MarcosCosmos
       <h1>Assets belong to: </h1>
-      <a href="https://store.steampowered.com/app/2868840/Slay_the_Spire_2/">
-        <SpSpireImage path="ui/misc/icon_1024" />
-        Slay the Spire 2
-      </a>{" "}
-      (&copy; <a href="http://megacrit.com/">MegaCrit</a>)
+      <SpSpireImage path="ui/misc/icon_1024" />
+      Slay the Spire 2 (&copy; MegaCrit)
       <h1>Assets & API provided via: </h1>
-      <a href="https://spire-codex.com/">
-        <img src="https://spire-codex.com/spire-codex-white-final.png" />
-        Spire Codex
-      </a>
+      <img src="https://spire-codex.com/spire-codex-white-final.png" />
+      Spire Codex
     </>
   );
   return (
-    <SpMenuItem className="credits" aria-label={label} tooltip={tooltip}>
+    <SpMenuItem
+      className="credits"
+      disabled
+      aria-label={label}
+      tooltip={tooltip}
+    >
       <SpPower path="powers/subroutine" />
     </SpMenuItem>
   );
